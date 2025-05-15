76b49962b78251e4219c44824da9dc74a4884675
sandyhouston <sandy.houston@rmit.edu.au>
2025-05-15 16:50:00 +1000

    feature: Added Life science to hamburger nav

--- picostrap5-child-base/header.php
+++ picostrap5-child-base/header.php
@@ -201,6 +201,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 										<li><a href="/art-and-design/">Art and design</a></li>
 										<li><a href="/chemistry/">Chemistry</a></li>
 										<li><a href="/law/">Law</a></li>
+                                        <li><a href="/life-science/">Life science</a></li>
 										<li><a href="/maths-statistics/">Mathematics and statistics</a></li>
 										<li><a href="/nursing/">Nursing</a></li>
 										<li><a href="/physics/">Physics</a></li>
