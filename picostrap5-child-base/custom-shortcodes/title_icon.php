b1c570bb14b5462883514a901927538e724adc04
sandyhouston <157427931+sandyhouston@users.noreply.github.com>
2025-07-23 17:15:30 +1000

    feature: minor-changes to alt tag, add alt tag only function

--- picostrap5-child-base/custom-shortcodes/title_icon.php
+++ picostrap5-child-base/custom-shortcodes/title_icon.php
@@ -9,17 +9,23 @@
 //
 //  $atts:      heading-tag    Specifies the heading level (h1-h6), default is 'h2'.
 //              type           CSS class name for distinguishing styles.
-//							   aboriginal-flag, torres-strait-flag, quiz are 
+//							   aboriginal-flag, torres-strait-flag, quiz-icon are 
 //			   				   predefined values, images etc. coded into css
 //
 //              img            (very much optional) URL of the icon image for the light mode.
 //              img-dark       (very much optional) URL of the icon image for the dark mode.
-//              alt            (very much optional) Alternate text for the icon image.
+//              alt            (optional) Alternate text for the icon image.
 //
 //  shortcode:  [title-icon]
-//	usage:			
-//  [title-icon heading-tag='h3' type='quiz-icon' img='https://path.to/light-icon.svg' img-dark='https://path.to/dark-icon.svg' alt='Quiz Icon']Your Heading Content[/title-icon]
+//	usage:	
+//
+//  [title-icon heading-tag='h3' type='aboriginal-flag']Your heading content[/title-icon]
 //	Expected output:
+//  <h3 class="title-icon aboriginal-flag">Your heading here</h3>
+//		
+//  [title-icon heading-tag='h3' type='my-type' img='https://path.to/light-icon.svg' img-dark='https://path.to/dark-icon.svg' alt='Quiz Icon']Your Heading Content[/title-icon]
+//	Expected output:
+//
 //  <style>
 //      .quiz-icon::before { 
 //          background-image: url("https://path.to/light-icon.svg"); 
@@ -31,7 +37,7 @@
 //          } 
 //      }
 //  </style>
-//  <h3 class="title-icon quiz-icon">Your Heading Content</h3>
+//  <h3 class="title-icon my-type">Your Heading Content</h3>
 
 
 function title_icon($atts, $content = null) {
@@ -40,7 +46,8 @@ function title_icon($atts, $content = null) {
         'type' => '',
 		'img' => '',
 		'img-dark' => '',
-        'alt' => ''
+        'alt' => '',
+        'id' => ''
     );
     
     //merges user-defined attributes with a set of default values ($default)
@@ -60,13 +67,26 @@ function title_icon($atts, $content = null) {
         }
     }
 
+    $myId ='';
+    if($a['id'] != '')
+    {
+        $myId = ' id="' . $a['id'] . '" ';
+    }
+
     $type = $a['type'];
-    $heading .= '<' . $headingTag . ' class="title-icon ' . $type . '">';
+    $heading .= '<' . $headingTag . ' class="title-icon ' . $type .'"' . $myId .'>';
 	$heading .= $content . '</' . $headingTag . '>';
 
 	$style = '';
 
-	if($a['img'] != '') {
+    if($a['alt'] != '' && $a['img'] == '') {
+        $style = '<style>';
+		$style .= '.' . $a['type'] .'::before { ' ;
+        $style .= "content: '' / '" . $a['alt'] . "'; ";
+		$style .= ' } ';
+        $style .=  '</style>';
+    }
+	elseif($a['img'] != '') {
 		
 		$style = '<style>';
 
